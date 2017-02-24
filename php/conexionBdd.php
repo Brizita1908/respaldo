5952985648f04aed1ffec0368e58f5df72fb8ac5
Brizita1908 <brigatita1795@hotmail.com>
2017-02-24 10:18:24 -0500

    edicion

--- php/conexionBdd.php
+++
@@ -1,5 +0,0 @@
-<?php
-//Conexion a la bdd asesoressa
-$conn = new mysqli('localhost' , 'root' , '', 'asesoressa');
-    if ($conn->connect_error) die($conn ->connect_error);
-?>
--- src/AsesoresSABundle/AsesoresSABundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AsesoresSABundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AsesoresSABundle extends Bundle
-{
-}
